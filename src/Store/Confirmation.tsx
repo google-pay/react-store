7a351c44dd69dba094b8292cc057a55f210533ab
Soc Sieng <socsieng@google.com>
2020-05-21 13:01:45 -0700

    Fix up styling for the confirmation page

--- src/Store/Confirmation.tsx
+++ src/Store/Confirmation.tsx
@@ -11,15 +11,17 @@ const Confirmation: React.FC<Props> = (props) => {
 
   return (
     <div className="Confirmation">
-      <Typography variant="h4">Success</Typography>
-      <Typography variant="body2">
-        Congratulations, on your purchase. The order has been received and is being processed.
-      </Typography>
-      <Typography variant="body2">
-        <em>This is for demo purposes only. You have not been charged, the order is not being processed.</em>
-      </Typography>
-      <div className="buttons">
-        <Button variant="text" onClick={() => history.push('/')}>Continue Shopping</Button>
+      <div className="content">
+        <Typography variant="h4">Success</Typography>
+        <Typography variant="body2">
+          Congratulations, on your purchase. The order has been received and is being processed.
+        </Typography>
+        <Typography variant="body2">
+          <em>This is for demo purposes only. You have not been charged, the order is not being processed.</em>
+        </Typography>
+        <div className="buttons">
+          <Button variant="text" onClick={() => history.push('/')}>Continue Shopping</Button>
+        </div>
       </div>
     </div>
   );
